c7b63a6e05dfc7078c771777ce3e76e55dd0e3a9
logan-22-99 <logan00029@gmail.com>
2025-03-20 10:18:53 +0530

    new branch created

---
+++ cypress/e2e/orange.cy.js
@@ -0,0 +1,8 @@
+describe('test suite created for login',()=>{
+    it('login orangehrm',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('[type="submit"]').click()
+    })
+})
